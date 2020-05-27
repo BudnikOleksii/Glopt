304f7958c9f5dfa9366a8f6c3340bb4455447acd
Alex <budnikoleksii@gmail.com>
2020-05-27 09:51:30 +0300

    prepared for work

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const btn = document.querySelector('button');
+
+    btn.addEventListener('click', () => {
+        btn.style.display = 'none';
+    });
+}); 
